7b804c0cea7ac222dd97ff05ae44b9b4679595d9
Dmitry Litokh <dmitry@MacBook-Pro-Dmitry.local>
2020-11-25 22:29:24 +1000

    new version

--- netmap.go
+++ netmap.go
@@ -10,17 +10,17 @@ import (
     "flag"
     "net"
     "fmt"
-	"strings"
-	"os/exec"
-	"sync"
-	"encoding/json"
+    "strings"
+    "os/exec"
+    "sync"
+    "encoding/json"
     "github.com/naoina/toml"
-	"gopkg.in/natefinch/lumberjack.v2"
-	"github.com/ltkh/telegraf-netmap/internal/http"
+    "gopkg.in/natefinch/lumberjack.v2"
+    "github.com/ltkh/telegraf-netmap/internal/http"
 )
 
 type Config struct {
-	NetMap      http.HTTP        `toml:"netmap"`
+    NetMap      http.HTTP        `toml:"netmap"`
 }
 
 // NetResponse struct
@@ -28,8 +28,8 @@ type NetResponse struct {
     Address     string           `toml:"adress"`
     Timeout     time.Duration    `toml:"timeout"`
     ReadTimeout time.Duration    `toml:"read_timeout"`
-	Protocol    string           `toml:"protocol"`
-	TimeoutCmd  string           `toml:"timeout_cmd"`
+    Protocol    string           `toml:"protocol"`
+    TimeoutCmd  string           `toml:"timeout_cmd"`
 }
 
 // TCPGather will execute if there are TCP tests defined in the configuration.
@@ -38,20 +38,20 @@ func (n *NetResponse) TCPGather() (int, float64) {
     // Start Timer
     start := time.Now()
     // Connecting
-	conn, err := net.DialTimeout("tcp", n.Address, n.Timeout)
+    conn, err := net.DialTimeout("tcp", n.Address, n.Timeout)
     // Stop timer
     responseTime := time.Since(start).Seconds()
     // Handle error
-	if err != nil {
-		log.Printf("[error] %v - %v", n.Address, err)
+    if err != nil {
+        log.Printf("[error] %v - %v", n.Address, err)
 
-		if e, ok := err.(net.Error); ok && e.Timeout() {
-			return 1, responseTime
-		}
-		return 2, responseTime
-	}
-	
-	defer conn.Close()
+        if e, ok := err.(net.Error); ok && e.Timeout() {
+            return 1, responseTime
+        }
+        return 2, responseTime
+    }
+    
+    defer conn.Close()
     
     return 0, responseTime
 }
@@ -73,21 +73,21 @@ func addFields(measurement string, fields map[string]interface{}, tags map[strin
 }
 
 func runCommand(cmd string) error {
-	fmt.Printf("[info] running %s\n", cmd)
-	var c *exec.Cmd
-	if runtime.GOOS == "windows" {
-		c = exec.Command("cmd", "/C", cmd)
-	} else {
-		c = exec.Command("/bin/sh", "-c", cmd)
-	}
+    log.Printf("[info] running %s\n", cmd)
+    var c *exec.Cmd
+    if runtime.GOOS == "windows" {
+        c = exec.Command("cmd", "/C", cmd)
+    } else {
+        c = exec.Command("/bin/sh", "-c", cmd)
+    }
 
-	output, err := c.CombinedOutput()
-	if err != nil {
-		fmt.Printf("[error] %q\n", string(output))
-		return err
-	}
-	fmt.Printf("[info] %q\n", string(output))
-	return nil
+    output, err := c.CombinedOutput()
+    if err != nil {
+        log.Printf("[error] %q\n", string(output))
+        return err
+    }
+    log.Printf("[info] %q\n", string(output))
+    return nil
 }
 
 func main() {
@@ -97,64 +97,67 @@ func main() {
 
     // Command-line flag parsing
     cfFile          := flag.String("config", "", "config file")
-	lgFile          := flag.String("logfile", "", "log file")
-	interval        := flag.Int("interval", 30, "interval")
+    lgFile          := flag.String("logfile", "", "log file")
+    interval        := flag.Int("interval", 30, "interval")
+    plugin          := flag.String("plugin", "", "plugin")
     log_max_size    := flag.Int("log_max_size", 1, "log max size") 
     log_max_backups := flag.Int("log_max_backups", 3, "log max backups")
     log_max_age     := flag.Int("log_max_age", 10, "log max age")
     log_compress    := flag.Bool("log_compress", true, "log compress")
     flag.Parse()
 
-	// Logging settings
-    log.SetOutput(&lumberjack.Logger{
-        Filename:   *lgFile,
-        MaxSize:    *log_max_size,    // megabytes after which new file is created
-        MaxBackups: *log_max_backups, // number of backups
-        MaxAge:     *log_max_age,     // days
-        Compress:   *log_compress,    // using gzip
-	})
-	
-	//program completion signal processing
-	c := make(chan os.Signal, 2)
-	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
-	go func() {
-		<- c
-		log.Print("[info] netmap stopped")
-		os.Exit(0)
-	}()
+    // Logging settings
+    if *lgFile != "" || *plugin != "" {
+        log.SetOutput(&lumberjack.Logger{
+            Filename:   *lgFile,
+            MaxSize:    *log_max_size,    // megabytes after which new file is created
+            MaxBackups: *log_max_backups, // number of backups
+            MaxAge:     *log_max_age,     // days
+            Compress:   *log_compress,    // using gzip
+        })
+    }
+    
+    //program completion signal processing
+    c := make(chan os.Signal, 2)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+    go func() {
+        <- c
+        log.Print("[info] netmap stopped")
+        os.Exit(0)
+    }()
 
-	// Loading configuration file
-	f, err := os.Open(*cfFile)
-	if err != nil {
-		log.Fatalf("[error] %v", err)
-	}
-	defer f.Close()
-	
-	var cfg Config
-	if err := toml.NewDecoder(f).Decode(&cfg); err != nil {
-		log.Fatalf("[error] %v", err)
-	}
+    // Loading configuration file
+    f, err := os.Open(*cfFile)
+    if err != nil {
+        log.Fatalf("[error] %v", err)
+    }
+    defer f.Close()
+    
+    var cfg Config
+    if err := toml.NewDecoder(f).Decode(&cfg); err != nil {
+        log.Fatalf("[error] %v", err)
+    }
 
-	log.Print("[info] netmap started -_-")
+    log.Print("[info] netmap started -_-")
 
     // Daemon mode
     for {
 
-		var wg sync.WaitGroup
-		var nr []NetResponse
+        var wg sync.WaitGroup
+        var nr []NetResponse
 
-		h := http.New(cfg.NetMap)
-		body, err := h.GatherURL()
-		if err != nil {
-			log.Printf("[error] %v", err)
-		} else {
-			if err := json.Unmarshal(body, &nr); err != nil {
-				log.Printf("[error] error reading json from response body: %s", err)
-			}
-		}
-		
-		for _, n := range nr {
-			wg.Add(1)
+        h := http.New(cfg.NetMap)
+        body, err := h.GatherURL()
+        if err != nil {
+            log.Printf("[error] %v", err)
+        } else {
+            if err := json.Unmarshal(body, &nr); err != nil {
+                log.Printf("[error] error reading json from response body: %s", err)
+            }
+        }
+        
+        for _, n := range nr {
+            wg.Add(1)
         
             go func(n NetResponse) {
                 defer wg.Done()
@@ -175,11 +178,11 @@ func main() {
                 }
                 if host == "" {
                     n.Address = "localhost:" + port
-				}
-				if port == "" {
-					log.Print("[error] bad port")
-					return
-				}
+                }
+                if port == "" {
+                    log.Print("[error] bad port")
+                    return
+                }
 
                 // Prepare data
                 tags := map[string]string{"server": host, "port": port}
@@ -188,35 +191,37 @@ func main() {
                 // Gather data
                 if n.Protocol == "tcp" {
 
-					result, response := n.TCPGather()
-					
-					tags["protocol"] = n.Protocol
-					fields["result_code"] = result
-					fields["response_time"] = response
-					
-					//
+                    result, response := n.TCPGather()
+                    
+                    tags["protocol"] = n.Protocol
+                    fields["result_code"] = result
+                    fields["response_time"] = response
+                    
+                    //
 
-					//runCommand(fmt.Sprintf("traceroute -p %d %s", port, host))
+                    //runCommand(fmt.Sprintf("traceroute -p %d %s", port, host))
 
                 } else if n.Protocol == "udp" {
                     //returnTags, fields = n.UDPGather()
                     //tags["protocol"] = "udp"
                 } else {
-					log.Print("[error] bad protocol")
-					return
+                    log.Print("[error] bad protocol")
+                    return
                 }
 
                 // Add metrics
-                fmt.Printf("%s\n", addFields("netmap", fields, tags))
+                if *plugin == "telegraf" {
+                    fmt.Printf("%s\n", addFields("netmap", fields, tags))
+                }
 
-			}(n)
-		}
+            }(n)
+        }
 
-		wg.Wait()
-		
-		time.Sleep(time.Duration(*interval) * time.Second)
+        wg.Wait()
+        
+        time.Sleep(time.Duration(*interval) * time.Second)
 
-	}
+    }
 
 }
 
